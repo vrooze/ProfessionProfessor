2fdcdbf1fd2738413ab90bfd295b459eb603fbfe
Mark Wilson <drviruz@gmail.com>
2022-09-29 12:07:34 +0100

    Added release builder and ignored release zips

---
+++ build-release.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+RELEASE_NAME="ProfessionsProfessor"
+
+echo "Building Release zip..."
+rm -f ${RELEASE_NAME}.zip
+zip -r ${RELEASE_NAME}.zip Libs *.toc *.lua
+echo Done!
+
+# Open in gui for uploading
+nemo .
